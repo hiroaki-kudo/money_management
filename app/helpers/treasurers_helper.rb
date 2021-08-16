cf9c11bfe9eee2f52c9287b72d52b950f626cd39
kudo_hiroaki <pikoniconico26@gmail.com>
2021-08-16 23:52:22 +0900

    treasurerのCRUD機能作成

--- app/helpers/treasurers_helper.rb
+++ app/helpers/treasurers_helper.rb
@@ -1,2 +1,9 @@
 module TreasurersHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      confirm_treasurers_path
+    elsif action_name == 'edit'
+      treasurer_path
+    end
+  end
 end
--- app/models/treasurer.rb
+++ app/models/treasurer.rb
@@ -1,2 +1,3 @@
 class Treasurer < ApplicationRecord
+  validates :category_id, :use_what, presence: true
 end
