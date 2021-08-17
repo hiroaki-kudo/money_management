6063247d43a886a3d1dc012c0330055293c804e6
kudo_hiroaki <pikoniconico26@gmail.com>
2021-08-17 22:57:12 +0900

    お気に入りにした情報をお気に入り一覧に表示できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:index, :create, :destroy]
 end
