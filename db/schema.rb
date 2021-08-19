26b9405198ddfefe71897cbf3a01c82f0ef0df84
kudo_hiroaki <pikoniconico26@gmail.com>
2021-08-19 20:03:55 +0900

    parentテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_110150) do
+ActiveRecord::Schema.define(version: 2021_08_19_110320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2021_08_19_110150) do
     t.index ["user_id"], name: "index_kids_on_user_id"
   end
 
+  create_table "parents", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_parents_on_user_id"
+  end
+
   create_table "treasurers", force: :cascade do |t|
     t.date "use_date_at"
     t.integer "category_id"
@@ -67,5 +75,6 @@ ActiveRecord::Schema.define(version: 2021_08_19_110150) do
   add_foreign_key "favorites", "treasurers"
   add_foreign_key "favorites", "users"
   add_foreign_key "kids", "users"
+  add_foreign_key "parents", "users"
   add_foreign_key "treasurers", "users"
 end
