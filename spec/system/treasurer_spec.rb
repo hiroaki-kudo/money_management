532c2815007126f5286a8bb04615ed5c6355c799
kudo_hiroaki <pikoniconico26@gmail.com>
2021-08-26 01:32:24 +0900

    子供の投稿した情報に対して親がコメントできるようにした

--- spec/system/treasurer_spec.rb
+++ spec/system/treasurer_spec.rb
@@ -315,7 +315,7 @@ RSpec.describe 'お金のメモ', type: :system do
 
 
   describe 'コメント投稿機能' do
-    context 'コメントした場合' do
+    context '子がコメントした場合' do
       it 'コメントした内容が詳細画面に表示される' do
         visit new_session_path
         fill_in 'Email', with: 'kodomo@kodomo.com'
@@ -335,6 +335,120 @@ RSpec.describe 'お金のメモ', type: :system do
         fill_in 'comment[content]', with: 'おいしかった'
         click_button 'commit'
         expect(page).to have_content 'おいしかった'
+      end
+    end
+    context '子がコメントを編集した場合' do
+      it '編集したコメントした内容が詳細画面に表示される' do
+        visit new_session_path
+        fill_in 'Email', with: 'kodomo@kodomo.com'
+        fill_in 'Password', with: '123qwe'
+        click_button 'commit'
+        click_on '使ったもの一覧'
+        click_on '使った物などを登録'
+        fill_in 'treasurer[use_date_at]', with: '002025-07-01'
+        select '②:食べ物・飲み物'
+        fill_in 'treasurer[use_what]', with: 'うまい棒'
+        fill_in 'treasurer[use_money]', with: 10
+        click_button 'commit'
+        click_button 'commit'
+        expect(page).to have_content 'お金の流れの一覧'
+        expect(page).to have_content 'うまい棒'
+        find('#detail_2').click
+        fill_in 'comment[content]', with: 'おいしかった'
+        click_button 'commit'
+        expect(page).to have_content 'おいしかった'
+        click_on 'コメント編集'
+        fill_in 'comment[content]', with: 'まずかった'
+        click_button 'commit'
+        expect(page).to have_content 'まずかった'
+      end
+    end
+    context '子コメントを削除した場合' do
+      it 'コメントした内容が削除される' do
+        visit new_session_path
+        fill_in 'Email', with: 'kodomo@kodomo.com'
+        fill_in 'Password', with: '123qwe'
+        click_button 'commit'
+        click_on '使ったもの一覧'
+        click_on '使った物などを登録'
+        fill_in 'treasurer[use_date_at]', with: '002025-07-01'
+        select '②:食べ物・飲み物'
+        fill_in 'treasurer[use_what]', with: 'うまい棒'
+        fill_in 'treasurer[use_money]', with: 10
+        click_button 'commit'
+        click_button 'commit'
+        expect(page).to have_content 'お金の流れの一覧'
+        expect(page).to have_content 'うまい棒'
+        find('#detail_2').click
+        fill_in 'comment[content]', with: 'おいしかった'
+        click_button 'commit'
+        expect(page).to have_content 'おいしかった'
+        click_on 'コメント削除'
+        expect(page).not_to have_content 'おいしかった'
+      end
+    end
+    context '親がコメントした場合' do
+      it 'コメントした内容が詳細画面に表示される' do
+        visit new_session_path
+        fill_in 'Email', with: 'oya@oya.com'
+        fill_in 'Password', with: '123qwe'
+        click_button 'commit'
+        click_on '子供一覧'
+        find('#detail_0').click
+        find('#detail_0').click
+        fill_in 'comment[content]', with: 'お勉強がんばってね'
+        click_button 'commit'
+        expect(page).to have_content 'お勉強がんばってね'
+      end
+    end
+    context '親がコメントを編集した場合' do
+      it '編集したコメントした内容が詳細画面に表示される' do
+        visit new_session_path
+        fill_in 'Email', with: 'oya@oya.com'
+        fill_in 'Password', with: '123qwe'
+        click_button 'commit'
+        click_on '使ったもの一覧'
+        click_on '使った物などを登録'
+        fill_in 'treasurer[use_date_at]', with: '002025-07-01'
+        select '②:食べ物・飲み物'
+        fill_in 'treasurer[use_what]', with: 'うまい棒'
+        fill_in 'treasurer[use_money]', with: 10
+        click_button 'commit'
+        click_button 'commit'
+        expect(page).to have_content 'お金の流れの一覧'
+        expect(page).to have_content 'うまい棒'
+        find('#detail_2').click
+        fill_in 'comment[content]', with: 'おいしかった'
+        click_button 'commit'
+        expect(page).to have_content 'おいしかった'
+        click_on 'コメント編集'
+        fill_in 'comment[content]', with: 'まずかった'
+        click_button 'commit'
+        expect(page).to have_content 'まずかった'
+      end
+    end
+    context '親がコメントを削除した場合' do
+      it 'コメントした内容が削除される' do
+        visit new_session_path
+        fill_in 'Email', with: 'oya@oya.com'
+        fill_in 'Password', with: '123qwe'
+        click_button 'commit'
+        click_on '使ったもの一覧'
+        click_on '使った物などを登録'
+        fill_in 'treasurer[use_date_at]', with: '002025-07-01'
+        select '②:食べ物・飲み物'
+        fill_in 'treasurer[use_what]', with: 'うまい棒'
+        fill_in 'treasurer[use_money]', with: 10
+        click_button 'commit'
+        click_button 'commit'
+        expect(page).to have_content 'お金の流れの一覧'
+        expect(page).to have_content 'うまい棒'
+        find('#detail_2').click
+        fill_in 'comment[content]', with: 'おいしかった'
+        click_button 'commit'
+        expect(page).to have_content 'おいしかった'
+        click_on 'コメント削除'
+        expect(page).not_to have_content 'おいしかった'
       end
     end
   end
