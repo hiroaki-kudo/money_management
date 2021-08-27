db6adb90f2a2b28a5afb553934d139ed2849e776
kudo_hiroaki <pikoniconico26@gmail.com>
2021-08-27 19:41:36 +0900

    出納情報の日付欄に未来の日付を入れられないようにした

--- app/models/treasurer.rb
+++ app/models/treasurer.rb
@@ -9,4 +9,12 @@ class Treasurer < ApplicationRecord
   belongs_to_active_hash :category
   validates :use_money, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 99999}
   validates :get_money, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 99999}
+
+  validate :day_after_today
+  def day_after_today
+    unless use_date_at == nil
+      errors.add(:use_date_at, 'は今日を入れて過去の日付を入力して下さい') if use_date_at > Date.today
+    end
+  end
+
 end
